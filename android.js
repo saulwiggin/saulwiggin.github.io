dd68eba8a6d220fed267a22a23c83c474a7cd039
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-21 14:14:50 +0100

    full android javascript

--- android.js
+++ android.js
@@ -1,33 +1,37 @@
-if (window.innerWidth && window.innerWidth <= 480) {
-	$(document).ready(function(){
-		$('#header ul').addclass('hide');
-		$('#header').append('<div class="leftbutton"
-		$('#header').append('<div class="leftButton"
-			onClick="toggleMenu()">Menu</div>');
-		});
-	function toggleMenu() {
-		$('#header ul').toggleClass('hide');
-		$('#header .leftButton').toggleClass('pressed');
-	}
-}
+var hist = [];
+var startUrl = 'index.html';
 $(document).ready(function(){
-	loadPage();
+	loadPage(startUrl);
 });
 function loadPage(url) {
 	$('body').append('<div id="progress">Loading...</div>');
-	if (url == undefined) {
-		$('#container').load('index.html #header ul', hijackLinks);
+	scrollTo(0,0);
+	if (url == startUrl) {
+		var element = ' #header ul';
 	} else {
-		$('#contianer').load(url + ' #content', hijackLinks);
+		var element = ' #content';
 	}
-}
-function hijackLinks() {
-	$('#container a').click(function(e){
-		e.prventDefault();
-		loadPage(e.target.href);
-	});
-	var title = $('h2').html() || 'Hello!';
-	$('h1').html(title);
-	$('h2').remove();
-	$('#progress').remove();
-}
+		$('#container').lead(url + element, function(){
+			var title = $('h2').html() || 'Hello!';
+			$('h1').html(title);
+			$('h2').remove();
+			$('.leftButton').remove();
+			hist.unshift({'url':url, 'title':title});
+			if ( hist.length > 1) {
+				$('#header').append('<div class="leftButton">'+hist[1].title+'</div>');
+				$('#header .leftButton').click(function(){
+					var thisPage = hist.shift();
+					var previousPage = hist.shift();
+					loadPage(previousPage.url);
+					});
+				}
+				$('#container a').click(function(e){
+					var url = e.target.href;
+					if (url.match(/saulwiggin.github.io/)){
+						e.preventDefault();
+						loadPage(url);
+					}
+				});
+				$('#progress').remove();
+			});
+		}
