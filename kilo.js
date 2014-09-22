a6b835ce14fa3b40109ef22a7d24d78922dd1c56
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-22 21:23:54 +0100

    create entry in javascript

--- kilo.js
+++ kilo.js
@@ -1,4 +1,6 @@
+var db;
 $(document).ready(function(){
+	$('#createEntry form').submit(createEntry);
 	$('#settings form').submit(saveSettings);
 	$('#settings').bind('pageAnimationStart', loadSettings);
 	$('#dates li a').click(function(){
@@ -11,3 +13,36 @@ $(document).ready(function(){
 		refreshEntries();
 	});
 });
+var shortName = 'Kilo';
+var version = '1.0';
+var displayName = 'Kilo';
+var maxSize = 65536;
+db = openDatabase(shortName, version, displayName, maxSize);
+db.transaction(
+	function(transaction) {
+		transaction.executeSql(
+			'CREATE TABLE IF NOT EXISTS entries ' +
+			' (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+			' date DATE NOT NULL, food TEXT NOT NULL, ' +
+			' calories INTEGER NOT NULL );'
+		);
+	}
+);
+function createEntry() {
+		var date = sessionStorage.currentDate;
+		var calories = $('#calories').val();
+		var food = $('#food').val();
+		db.transaction(
+			function(transaction) {
+				transaction.executeSql(
+					'INSERT INTO entries (date, calories, food) VALUES (?, ?, ?);',
+					function(){
+						refreshEntries();
+						jQT.goBack();
+						},
+						errorHandler
+					);
+				}
+			);
+			return false;
+		}
